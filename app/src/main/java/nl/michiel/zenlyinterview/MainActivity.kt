f977ff8cf21f726c6c4e387b10252f2b2b0b2a6f
Michiel van Liempt <michiel.v.liempt@gmail.com>
2020-10-28 22:55:12 +0100

    added view pager

--- app/src/main/java/nl/michiel/zenlyinterview/MainActivity.kt
+++ app/src/main/java/nl/michiel/zenlyinterview/MainActivity.kt
@@ -1,11 +1,32 @@
 package nl.michiel.zenlyinterview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.PagerAdapter
+import kotlinx.android.synthetic.main.activity_main.pagerView
+import androidx.fragment.app.FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT as BEHAVIOR
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        pagerView.adapter = initAdapter()
     }
+
+    private fun initAdapter(): PagerAdapter {
+        val pages = listOf(
+            SimpleFragment(R.layout.page1),
+            SimpleFragment(R.layout.page2),
+            SimpleFragment(R.layout.page3)
+        )
+
+        return object: FragmentStatePagerAdapter(supportFragmentManager, BEHAVIOR) {
+            override fun getCount(): Int = pages.size
+            override fun getItem(position: Int): Fragment = pages[position]
+        }
+    }
+
 }
---
+++ app/src/main/java/nl/michiel/zenlyinterview/SimpleFragment.kt
@@ -0,0 +1,19 @@
+package nl.michiel.zenlyinterview
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+
+class SimpleFragment(@LayoutRes private val layout: Int): Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(layout, container, false)
+    }
+}
