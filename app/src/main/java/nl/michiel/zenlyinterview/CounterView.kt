9be8a3245828a60fbcdfedad2137a291c68a5eb8
Michiel van Liempt <michiel.v.liempt@gmail.com>
2020-10-29 15:34:41 +0100

    text animates correctly

--- app/src/main/java/nl/michiel/zenlyinterview/CounterView.kt
+++ app/src/main/java/nl/michiel/zenlyinterview/CounterView.kt
@@ -4,35 +4,45 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.animation.AccelerateDecelerateInterpolator
+import androidx.annotation.VisibleForTesting
 import androidx.appcompat.widget.AppCompatTextView
+import timber.log.Timber
 import java.lang.System.currentTimeMillis
 import java.util.Random
+import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
 
 class CounterView(
-    context: Context,
-    attrs: AttributeSet? = null
+    context: Context, attrs: AttributeSet? = null
 ) : AppCompatTextView(context, attrs) {
 
     private var animationRange = 0
     private var animationDuration = 0f
     private var animationStartTime = 0L
+    private var textHeight = 0
 
+    @VisibleForTesting
     var interpolator = AccelerateDecelerateInterpolator()
 
+    @VisibleForTesting
     var number = 0
         set(value) {
             field = value
             stopAnimation()
-            invalidate()
+//            invalidate()
         }
 
     init {
         val rng = Random()
-        setOnClickListener { animateTo(rng.nextInt(1000)) }
+        setOnClickListener { animateTo(100 + rng.nextInt(800)) }
+        minLines = 2
+        maxLines = 2
     }
 
+    @VisibleForTesting
     fun animateTo(number: Int, time: Int = 1500) {
         animationRange = number - this.number
         animationStartTime = currentTimeMillis()
@@ -47,6 +57,10 @@ class CounterView(
         } catch (error: Throwable) {
             0
         }
+
+        val bounds = Rect()
+        paint.getTextBounds(text, 0, text.length, bounds)
+        textHeight = bounds.height()
     }
 
     private fun stopAnimation() {
@@ -55,32 +69,50 @@ class CounterView(
     }
 
     override fun onDraw(canvas: Canvas?) {
-
+        Timber.d(".")
         val animationTime = currentTimeMillis() - animationStartTime
-        propagateAnimation(animationTime)
+        val currentValue = propagateAnimation(animationTime)
+        val currentNumber = currentValue.roundToInt()
+        val offset = (currentValue - currentNumber) * lineHeight
 
+        val lineHeight = paint.fontMetrics.let { it.ascent - it.descent }.absoluteValue
+
+        val centeredTextY = (height + textHeight) / 2f
+        canvas?.drawColor(Color.LTGRAY)
+
+        drawOutlinedText(currentNumber.toString(), canvas, centeredTextY - offset)
+        drawOutlinedText((currentNumber-1).toString(), canvas, centeredTextY - lineHeight - offset)
+        drawOutlinedText((currentNumber+1).toString(), canvas, centeredTextY + lineHeight - offset)
+
+        if (animationTime < animationDuration) {
+            Timber.d("$animationTime   $animationDuration")
+            invalidate()
+        }
+    }
+
+    private fun drawOutlinedText(text: String, canvas: Canvas?, centeredTextY: Float) {
         paint.style = Paint.Style.FILL
-        setTextColor(Color.WHITE)
-        super.onDraw(canvas)
+        paint.color = Color.WHITE
+        canvas?.drawText(text, 0f, centeredTextY, paint)
 
         paint.style = Paint.Style.STROKE
         paint.strokeWidth = 1f
-        setTextColor(Color.BLACK)
-        super.onDraw(canvas)
-
-        if (animationTime < animationDuration) invalidate()
+        paint.color = Color.BLACK
+        canvas?.drawText(text, 0f, centeredTextY, paint)
     }
 
-
-    private fun propagateAnimation(animationTime: Long) {
-        text = if (animationTime < animationDuration) {
+    private fun propagateAnimation(animationTime: Long): Float {
+        return if (animationTime < animationDuration) {
             val animationValue = interpolator.getInterpolation(animationTime / animationDuration)
+            Timber.d("anim = $animationValue")
             val amount = number + (animationRange * animationValue)
-            amount.toInt().toString()
+            amount
         } else {
-            number += animationRange
+            if (animationRange != 0) {
+                number += animationRange
+            }
             animationRange = 0
-            number.toString()
+            number.toFloat()
         }
     }
 
