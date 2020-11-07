49072d25cb0d964f76b864eb551e49a3a9f2336a
Michiel van Liempt <michiel.v.liempt@gmail.com>
2020-11-07 11:16:20 +0100

    don't show old results

--- photogrid/src/main/java/nl/michiel/photogrid/data/PhotoRepository.kt
+++ photogrid/src/main/java/nl/michiel/photogrid/data/PhotoRepository.kt
@@ -2,6 +2,11 @@ package nl.michiel.photogrid.data
 
 class PhotoRepository(private val service: RandomUserService) {
     suspend fun loadPhotoList(): List<Photo> =
+        try {
             service.loadUserList().results.map { Photo(it.picture.large) }
+        } catch (error: Throwable) {
+            error.printStackTrace()
+            emptyList()
+        }
 }
 
--- photogrid/src/main/java/nl/michiel/photogrid/ui/photogrid/PhotoItem.kt
+++ photogrid/src/main/java/nl/michiel/photogrid/ui/photogrid/PhotoItem.kt
@@ -1,14 +1,10 @@
 package nl.michiel.photogrid.ui.photogrid
 
-import android.net.Uri
 import android.widget.ImageView
 import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
 import com.xwray.groupie.kotlinandroidextensions.Item
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import nl.michiel.photogrid.R
 import nl.michiel.photogrid.data.Photo
 import nl.michiel.photogrid.data.PhotoLoader
@@ -22,9 +18,17 @@ class PhotoItem(
     override fun getLayout(): Int = R.layout.photo_grid_item
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
         Timber.d("showing ${photo.url}")
-        val imageView = viewHolder.itemView as ImageView
-        scope.launch {
-            imageView.setImageBitmap(loader.get(photo.url))
+        with(viewHolder.itemView as ImageView) {
+            setImageDrawable(null)
+            setTag(R.id.url, photo.url)
+            scope.launch {
+                val bitmap = loader.get(photo.url)
+                if (getTag(R.id.url) == photo.url) {
+                    setImageBitmap(bitmap)
+                } else {
+                    Timber.d("ignored out-of-date result")
+                }
+            }
         }
     }
 }
